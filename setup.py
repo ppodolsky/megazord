c03f103e75c30f776420aa1794bd30442b9ba7e1
PashaPodolsky <ppodolsky@me.com>
2015-11-28 12:26:46 +0300

    fix

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='megazord',
-      version='0.17.10',
+      version='0.17.11',
       description='Code Management System',
       author='Pasha Podolsky',
       author_email='ppodolsky@me.com',
